b7a082797e4dddcaf750be1b97077a2446843e20
dlutz494 <dlutz494@gmail.com>
2022-03-23 16:39:48 -0400

    Initial commit

---
+++ resources/views/page.blade.php
@@ -0,0 +1,9 @@
+<!doctype html>
+
+<title>My first app</title>
+<link rel="stylesheet" href="resources/css/app.css">
+<script src="resources/js/app.js"></script>
+
+<body>
+    <h1>First Page</h1>
+</body>
