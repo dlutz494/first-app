5bf038aeaba4054b959d0b5fbe85894ed5c96bbc
dlutz494 <dlutz494@gmail.com>
2022-03-24 15:50:29 -0400

    Create task list

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,5 +5,9 @@
 <script src="resources/js/app.js"></script>
 
 <body>
-    <h1>Hello World</h1>
+    <h1>Things I want to do</h1>
+    <ul>
+        <li><a href="task">Keep practicing git stuff</a></li>
+        <li>Rewatch PHP and Laravel laracasts</li>
+    </ul>
 </body>
