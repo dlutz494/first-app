96fd5132c90668fd1d28c548321c4688e8af9082
dlutz494 <dlutz494@gmail.com>
2022-03-31 13:14:50 -0400

    revert most of the project to a fresh start

--- routes/web.php
+++ routes/web.php
@@ -16,25 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/tasks', function () {
-    return view('tasks');
-});
-
-Route::get('/tasks/{task}', function ($slug) {
-    $path = __DIR__ . "/../resources/tasks/{$slug}.html";
-
-    if (! file_exists($path)) {
-        return redirect('/');
-    }
-
-//    $task = cache()->remember("tasks.{$slug}", now()->addMinute(5), function () use ($path) {
-//        return file_get_contents($path);
-//    });
-
-    $task = file_get_contents($path);
-
-    return view('task', [
-        'task' => $task
-    ]);
-})->where('task', '[A-z_\-]+');
