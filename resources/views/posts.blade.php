870c43322b6fe799ec4ace9c8d76c7e351936be1
dlutz494 <dlutz494@gmail.com>
2022-04-18 16:06:16 -0400

    replace '<?= ?>' with {{}} and @

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,20 +4,20 @@
 <link rel="stylesheet" href="/app.css">
 
 <body>
-<?php foreach ($posts as $post) : ?>
+@foreach ($posts as $post)
 <article>
     <h1>
-        <a href="/posts/<?= $post->slug ?>">
-            <?= $post->title ?>
+        <a href="/posts/{{ $post->slug }}">
+            {{ $post->title }}
         </a>
     </h1>
     <p>
-        <?= date('Y M d', $post->date) ?>
+        {!! date('Y M d', $post->date) !!}
     </p>
 
     <div>
-        <?=$post->excerpt?>
+        {!! $post->excerpt !!}
     </div>
 </article>
-<?php endforeach; ?>
+@endforeach
 </body>
