672b5771105f4988495064096130033c8e0a507f
dlutz494 <dlutz494@gmail.com>
2022-05-11 08:52:53 -0400

    laravel 8 from scratch ep 16

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -34,12 +34,23 @@ class Post
                     $document->body(),
                     $document->date,
                     $document->slug,
-                ))->sortBy('date');
+                ))->sortByDesc('date');
         });
     }
 
     public static function find($slug)
     {
         return static::all()->firstWhere('slug', $slug);
+    }
+
+    public static function findOrFail($slug)
+    {
+        $post = static::find($slug);
+
+        if (! $post) {
+            throw new ModelNotFoundException();
+        }
+
+        return $post;
     }
 }
